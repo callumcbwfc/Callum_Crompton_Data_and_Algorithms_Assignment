12388d7d34eaff4d3ebbfddadcb69090aa7b4aa8
callumcbwfc <callumcrompton@hotmail.co.uk>
2026-04-27 11:05:42 +0100

    Program.cs has been set up from the starter code provided, original starter code was kept for reading the uk waypoints file.
    WayPoint.cs has been created with a basic skeleton with just private fields, allowing me to build on it later.

---
+++ WayPoint.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace StarterCode_WayPoints
+{
+    // WayPoint class for one GPS waypoint
+    // Starting this after looking at Week 3 Person/Book examples
+    public class WayPoint
+    {
+        // private fields - will add properties next
+        private string name;
+        private string code;
+        private string latitude;
+        private string longitude;
+        private int elevation;
+        private string description;
+
+        // Constructor will go here later
+    }
+}
