e500a192a786e0ccaaa9e427f8723a22d60ababc
callumcbwfc <callumcrompton@hotmail.co.uk>
2026-04-27 11:36:07 +0100

    Constructors added to the waypoints class. tested build, succeeded

--- WayPoint.cs
+++ WayPoint.cs
@@ -2,18 +2,30 @@
 
 namespace StarterCode_WayPoints
 {
-    // WayPoint class for one GPS waypoint
-    // Starting this after looking at Week 3 Person/Book examples
+    // WayPoint class for one GPS waypoint, storing data for a waypoints from the provided CSV file.
+    // Starting this after looking at Week 3 Powerpoints and  recalling previous lab session knowledge
     public class WayPoint
     {
-        // private fields - will add properties next
+        // private fields
         private string name;
         private string code;
         private string latitude;
         private string longitude;
-        private int elevation;
+        private int elevation; // measures the height in metres
         private string description;
 
-        // Constructor will go here later
+        // Constructor, will setup a new waypoint when its created in the future
+        public WayPoint(string name, string code, string latitude,
+                        string longitude, int elevation, string description)
+        {
+            this.name = name;
+            this.code = code;
+            this.latitude = latitude;
+            this.longitude = longitude;
+            this.elevation = elevation;
+            this.description = description;
+        }
+
+        // Getters and Setters will be added next
     }
 }
