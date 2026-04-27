918cecea5fd11ffdb23869eaaeb001890a3b60c3
callumcbwfc <callumcrompton@hotmail.co.uk>
2026-04-27 13:17:54 +0100

    WayPointStore Class skeleton added, to be expanded upon

--- WayPointStore.cs
+++ WayPointStore.cs
@@ -1,10 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace StarterCode_WayPoints
 {
-    internal class WayPointStore
+    // WaypointStore, holds all of the waypoints in an array
+    // Based on array-backed Stack and Queue examples from week 3 lecture powerpoint and lab session
+    public class WaypointStore
     {
+        private WayPoint[] waypoints;
+        private int count;   // how many waypoints added so far
+
+        // Constructor - creates the array of any given size
+        public WaypointStore(int maxSize)
+        {
+            waypoints = new WayPoint[maxSize];
+            count = 0;
+        }
+
+        // AddWaypoint method will be added next
     }
-}
+} 
